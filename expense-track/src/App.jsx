7bd0737545fec861e50f43e03dc9c201a10a24f9
bipin pokharel <pokharelbipin850@gmail.com>
2026-09-12 18:43:40 +0545

    added all the starting files

---
+++ expense-track/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { AddExpenseForm } from "./components/AddExpenseForm";
+import { CategoryChart } from "./components/CategoryChart";
+import { ExpenseList } from "./components/ExpenseList";
+import { FilterBar } from "./components/FilterBar";
+import { Summary } from "./components/Summary";
+
+function App() {
+  const [expenses, setExpenses] = useState([]);
+  const [filterCategory, setFilterCategory] = useState("all");
+
+  function addExpense(newExpense) {
+    setExpenses((prev) => [...prev, newExpense]);
+  }
+  function deleteExpense(id) {
+    setExpenses((prev) => prev.filter((expense) => expense.id !== id));
+  }
+  const filteredExpenses =
+    filterCategory === "all"
+      ? expenses
+      : expenses.filter((expense) => expense.category === filterCategory);
+
+  return (
+    <div className="app">
+      <h1>Expense Tracker</h1>
+      <Summary expenses={expenses} />
+      <AddExpenseForm onAddExpense={addExpense} />
+      <FilterBar
+        expenses={expenses}
+        filterCategory={filterCategory}
+        setFilterCategory={setFilterCategory}
+      />
+      <CategoryChart expenses={expenses} />
+      <ExpenseList
+        expenses={filteredExpenses}
+        onDeleteExpense={deleteExpense}
+      />
+    </div>
+  );
+}
+export default App;
---
+++ expense-track/src/components/AddExpenseForm.jsx
@@ -0,0 +1,5 @@
+import { useState } from "react";
+
+export function AddExpenseForm({ onAddExpense }) {
+  const [description, setDescription] = useState("");
+}
