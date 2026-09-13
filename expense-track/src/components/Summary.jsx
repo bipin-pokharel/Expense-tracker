8103f5fe4a2e0875d35b7df2d8f07cd4dd22ac8f
bipin pokharel <pokharelbipin850@gmail.com>
2026-09-14 00:53:32 +0545

    feat: implement Summary component to display balance, income, and expenses

--- expense-track/src/components/Summary.jsx
+++ expense-track/src/components/Summary.jsx
@@ -1,3 +1,22 @@
-export function Summary() {
-  return <div></div>;
+import {
+  getBalance,
+  getTotalExpenses,
+  getTotalIncome,
+} from "../utils/calculations";
+export function Summary({ expenses }) {
+  const balance = getBalance(expenses);
+  const income = getTotalIncome(expenses);
+  const expenseTotal = getTotalExpenses(expenses);
+
+  return (
+    <div className="summary">
+      <div className="summary-balance">Balance : ${balance.toFixed(2)}</div>
+      <div className="summary-row">
+        <span className="summary__income">Income:${income.toFixed(2)}</span>
+        <span className="summary__expenses">
+          Expenses:${expenseTotal.toFixed(2)}
+        </span>
+      </div>
+    </div>
+  );
 }
